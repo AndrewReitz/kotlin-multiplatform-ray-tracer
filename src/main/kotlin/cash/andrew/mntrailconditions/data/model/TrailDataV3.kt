2a99d7e3cc91b09d0666cdb56f5c659c904a663b
Andrew Reitz <aj.reitz@gmail.com>
2018-05-30 21:45:09 -0500

    Add Plurals
    For last updated days and hours

--- src/main/kotlin/cash/andrew/mntrailconditions/data/model/TrailDataV3.kt
+++ src/main/kotlin/cash/andrew/mntrailconditions/data/model/TrailDataV3.kt
@@ -1,11 +1,7 @@
 package cash.andrew.mntrailconditions.data.model
 
-import android.support.annotation.DrawableRes
-import cash.andrew.mntrailconditions.util.statusToResource
 import com.squareup.moshi.JsonClass
 import org.threeten.bp.Instant
-import org.threeten.bp.LocalDateTime
-import org.threeten.bp.temporal.ChronoUnit
 
 @JsonClass(generateAdapter = true)
 data class TrailDataV3(
@@ -22,13 +18,3 @@ data class TrailDataV3(
         val state: String?,
         val street: String?
 )
-
-val TrailDataV3.updatedAtString: String get() {
-    val now = LocalDateTime.now()
-    val days = ChronoUnit.DAYS.between(updatedAt, now)
-    val hours = ChronoUnit.HOURS.between(updatedAt, now) % 24
-    return if (days == 0L) "$hours hours ago" else "$days days $hours hours ago"
-}
-
-@get:DrawableRes
-val TrailDataV3.resourceId: Int get() = statusToResource(trailStatus ?: "")

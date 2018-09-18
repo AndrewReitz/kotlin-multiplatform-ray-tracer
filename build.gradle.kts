51990d4c2cc0d3106d401fa2b2f97d958eb877fa
Andrew Reitz <aj.reitz@gmail.com>
2018-09-18 14:11:33 -0500

    Update Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.2.60"
+    kotlin("jvm") version "1.2.70"
+    id("com.gradle.build-scan") version "1.15.2"
+}
+
+buildScan {
+    setTermsOfServiceUrl("https://gradle.com/terms-of-service")
+    setTermsOfServiceAgree("yes")
+    publishAlways()
 }
 
 allprojects {

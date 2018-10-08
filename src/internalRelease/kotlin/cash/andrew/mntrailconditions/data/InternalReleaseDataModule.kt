70dcd8103d42e850a9572d8ebb907bdd77b01440
Andrew Reitz <aj.reitz@gmail.com>
2018-10-08 16:56:55 -0500

    Remove Unused Files

--- src/internalRelease/kotlin/cash/andrew/mntrailconditions/data/InternalReleaseDataModule.kt
+++
@@ -1,7 +0,0 @@
-package cash.andrew.mntrailconditions.data
-
-import cash.andrew.mntrailconditions.data.api.InternalReleaseApiModule
-import dagger.Module
-
-@Module(includes = [InternalReleaseApiModule::class])
-object InternalReleaseDataModule

a610209766037bdf52630e0cce0f2f1e50b07452
Andrew <aj.reitz@gmail.com>
2020-05-02 22:17:07 -0500

    fixed typo?

--- raytracer-parsing/src/main/kotlin/raytracer/parsing/Parser.kt
+++ raytracer-parsing/src/main/kotlin/raytracer/parsing/Parser.kt
@@ -4,7 +4,6 @@ import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types
 import okio.Okio
-import org.omg.CORBA.Any
 import java.io.File
 
 class Parser {
